3f349ee2b64952615178d004a6eea2ee4b167b19
Melissaderrick <melissa-0613@hotmail.com>
2023-01-28 22:09:12 -0500

    first commit

---
+++ Week-06-DevTools_Debugging_and_Unit_Tests/warProject/main.js
@@ -0,0 +1,159 @@
+class Player {
+    constructor(name) {
+        this.name = name;
+        this.hand = [];
+        this.score = 0;
+    }
+}
+
+class Card {
+    constructor(rank, value, suit) {
+        this.rank = rank;
+        this.value = value;
+        this.suit = suit;
+    }
+}
+
+class Deck {
+    constructor() {
+        this.deck = [];
+
+        const suits = ['Spades', 'Hearts', 'Clubs', 'Diamonds'];
+        const ranks = ['2', '3', '4', '5', '6', '7', '8', '9', '10',
+        'Jack', 'Queen', 'King', 'Ace'];
+        
+        //Create each card in the deck by looping over the suits and ranks. 
+        //Values assigned from rank index low to high
+        for (let i = 0; i < suits.length; i++) {
+            for (let j = 0; j < ranks.length; j++) {
+                this.deck.push(new Card(ranks[j], j + 2, suits[i]));
+            }
+        }
+
+    }
+
+    // Randomly shuffles the deck, swapping card indexes - used shuffle funciton
+    shuffle() {
+        for (let i = this.deck.length - 1; i > 0; i--) {
+            let randomIndex = Math.floor(Math.random() * (i + 1));
+            let currentIndex = this.deck[i];
+            this.deck[i] = this.deck[randomIndex];
+            this.deck[randomIndex] = currentIndex;
+        }
+
+        return this.deck;
+    }
+
+    //Split deck to both players 26 cards per player
+    deal(firstPlayer, secondPlayer) {
+        firstPlayer.hand = [...this.deck.slice(0,26)];
+        secondPlayer.hand = [...this.deck.slice(26, 52)];
+    }
+}
+
+class Game {
+    constructor() {
+        this.players=[];
+    }
+    
+    // A prompt will ask you to assign the names of players. Will not accept null or empty string.
+    createPlayer(player) {
+        let name = prompt(`Enter name of player ${player}.`, `Player ${player}`);
+                
+        while (name == '' || name === null) {
+            name = prompt(`Who is playng? ${player}.`);
+        }
+        this.players.push(new Player(name));
+
+        const playerDiv = document.querySelector('#' + player);
+        playerDiv.textContent = name;
+    }
+
+    // Runs the game
+    start() {
+        document.querySelector('#startbutton').disabled = true;
+
+        this.createPlayer('one');
+        this.createPlayer('two');
+
+        const gameDeck = new Deck;
+        gameDeck.shuffle();
+        gameDeck.deal(this.players[0], this.players[1]);
+
+        this.playCards(this.players[0], this.players[1]);
+    }
+
+    // Deals the cards to the players
+    async playCards(playerOne, playerTwo) {
+        // Getters
+        const playerOneScoreUl = document.querySelector('.player-one-score');
+        const playerTwoScoreUl = document.querySelector('.player-two-score');
+        const compareScoresUl = document.querySelector('.score-list');
+        let playerOneTotalScore = document.querySelector('#player-one-total-score');
+        let playerTwoTotalScore = document.querySelector('#player-two-total-score');
+        let winner = document.querySelector('#winner');
+        
+        // Loop over each hand to determine score
+        for(let i = 0; i < this.players[0].hand.length; i++) {
+            let playerOneCard = playerOne.hand[i];
+            let playerTwoCard = playerTwo.hand[i];
+
+            this.printCardPlayerOne(playerOneScoreUl, playerOneCard);
+            this.printCardPlayerTwo(playerTwoScoreUl, playerTwoCard);
+
+            if (playerOneCard.value > playerTwoCard.value) {
+                this.printScore(`${playerOne.name} wins!`, compareScoresUl);
+                playerOne.score++;
+            } else if (playerOneCard.value < playerTwoCard.value) {
+                this.printScore(`${playerTwo.name} wins!`, compareScoresUl);
+                playerTwo.score++;
+            } else if (playerOneCard.value === playerTwoCard.value) {
+                this.printScore(`Its a draw! No points awarded.`, compareScoresUl);
+            }
+
+            playerOneTotalScore.innerHTML = `Score: ${playerOne.score}`;
+            playerTwoTotalScore.innerHTML = `Score: ${playerTwo.score}`;
+            await this.timer(600);
+        }
+
+        //Checks for winner.
+        if (playerOne.score > playerTwo.score) {
+            winner.innerHTML = `${playerOne.name} Wins!!!`;
+        } else if (playerOne.score < playerTwo.score) {
+            winner.innerHTML = `${playerTwo.name} Wins!!!`;
+        } else if (playerOne.score === playerTwo.score) {
+            winner.innerHTML = `Its a Draw!!! Nobody wins in war.`;
+        }
+
+        //Changes start button to reload button which will make it so you can play again
+        document.querySelector('#startbutton').setAttribute('onclick', 'location.reload();');
+        document.querySelector('#startbutton').disabled = false;
+        document.querySelector('#startbutton').innerHTML = 'Play Again?';
+    }
+
+    // Changed Players One's DOM
+    printCardPlayerOne(playerOneScoreUl, card) {
+        let playerOneScoreLi = document.createElement('li');
+        playerOneScoreLi.appendChild(document.createTextNode(card.rank + ' of ' + card.suit));
+        playerOneScoreUl.appendChild(playerOneScoreLi);
+    }
+
+    // Changes Players Two's DOM
+    printCardPlayerTwo(playerTwoScoreUl, card) {
+        let playerTwoScoreLi = document.createElement('li');
+        playerTwoScoreLi.appendChild(document.createTextNode(card.rank + ' of ' + card.suit));
+        playerTwoScoreUl.appendChild(playerTwoScoreLi);
+    }
+
+    // Changes Score Column DOM
+    printScore(winner, compareScoresUl) {
+        let compareScoresLi = document.createElement('li');
+        compareScoresLi.appendChild(document.createTextNode(winner));
+        compareScoresUl.appendChild(compareScoresLi);
+    }
+
+    // Helper async function to control flow
+    timer = ms => new Promise(res => setTimeout(res, ms));
+}
+
+let war = new Game;
